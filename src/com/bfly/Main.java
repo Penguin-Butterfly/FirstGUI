0726a909040cecc5405421d94450606745892fc4
Penguin-Butterfly <MattN55940@gmail.com>
2021-03-03 10:20:55 -0500

    Fixed Image Import for split menu button

--- src/com/bfly/Main.java
+++ src/com/bfly/Main.java
@@ -35,6 +35,7 @@ public class Main extends Application {
 
 
     static ArrayList<Image> images = new ArrayList<>();
+    static ArrayList<MenuItem> options = new ArrayList<>();
 
     public void start(Stage stage) throws FileNotFoundException {
         stage.setTitle("Maddie Bfly");
@@ -55,10 +56,44 @@ public class Main extends Application {
         images.add(bfly2);
         images.add(bfly3);
         images.add(bfly4);
-        imgNum = 4;
+        imgNum = images.size();
 
 
         //image selection
+
+
+
+        ImageView current = new ImageView();
+
+
+        //layout2.getChildren().add(current);
+        current.setImage(images.get(i));
+        current.setFitHeight(480);
+        current.setPreserveRatio(true);
+
+        //UI
+        /* get back to this later
+        ToggleButton tBtn1 = new ToggleButton("rewind");
+        ToggleButton tBtn2 = new ToggleButton("pause");
+        ToggleButton tBtn3 = new ToggleButton("forward");
+        */
+
+        Button tBtn1 = new Button("rewind");
+        Button tBtn2 = new Button("pause");
+        Button tBtn3 = new Button("forward");
+        SplitMenuButton selectImg = new SplitMenuButton();
+        selectImg.setText("select image");
+
+
+
+        MenuItem o0 = new MenuItem("1");
+        MenuItem o1 = new MenuItem("2");
+        MenuItem o2 = new MenuItem("3");
+        MenuItem o3 = new MenuItem("4");
+        MenuItem o4 = new MenuItem("5");
+        selectImg.getItems().addAll(o0,o1,o2,o3,o4);
+
+
 
         //Filechooser
 
@@ -76,37 +111,79 @@ public class Main extends Application {
                 fileNotFoundException.printStackTrace();
             }
             Image newImg = new Image(newFile);
-            images.add(newImg);
-            imgNum = images.size();
+            if(images.size()==10) {
+                images.add(newImg);
+                imgNum = images.size();
+                String s = String.valueOf(imgNum);
+                MenuItem o10 = new MenuItem(s);
+                selectImg.getItems().add(o10);
+                o10.setOnAction(actionEvent -> {
+                    i = 10;
+                    current.setImage(images.get(i));
+                    System.out.println(i);
+                });
+            }
+            if(images.size()==9) {
+                images.add(newImg);
+                imgNum = images.size();
+                String s = String.valueOf(imgNum);
+                MenuItem o9 = new MenuItem(s);
+                selectImg.getItems().add(o9);
+                o9.setOnAction(actionEvent -> {
+                    i = 9;
+                    current.setImage(images.get(i));
+                    System.out.println(i);
+                });
+            }
+            if(images.size()==8) {
+                images.add(newImg);
+                imgNum = images.size();
+                String s = String.valueOf(imgNum);
+                MenuItem o8 = new MenuItem(s);
+                selectImg.getItems().add(o8);
+                o8.setOnAction(actionEvent -> {
+                    i = 8;
+                    current.setImage(images.get(i));
+                    System.out.println(i);
+                });
+            }
+            if(images.size()==7) {
+                images.add(newImg);
+                imgNum = images.size();
+                String s = String.valueOf(imgNum);
+                MenuItem o7 = new MenuItem(s);
+                selectImg.getItems().add(o7);
+                o7.setOnAction(actionEvent -> {
+                    i = 7;
+                    current.setImage(images.get(i));
+                    System.out.println(i);
+                });
+            }
+            if(images.size()==6) {
+                images.add(newImg);
+                imgNum = images.size();
+                String s = String.valueOf(imgNum);
+                MenuItem o6 = new MenuItem(s);
+                selectImg.getItems().add(o6);
+                o6.setOnAction(actionEvent -> {
+                    i = 6;
+                    current.setImage(images.get(i));
+                    System.out.println(i);
+                });
+            }
+            if(images.size()==5) {
+                images.add(newImg);
+                imgNum = images.size();
+                String s = String.valueOf(imgNum);
+                MenuItem o5 = new MenuItem(s);
+                selectImg.getItems().add(o5);
+                o5.setOnAction(actionEvent -> {
+                    i = 5;
+                    current.setImage(images.get(i));
+                    System.out.println(i);
+                });
+            }
         });
-
-
-
-        ImageView current = new ImageView();
-        //layout2.getChildren().add(current);
-        current.setImage(images.get(i));
-
-
-        //UI
-        /* get back to this later
-        ToggleButton tBtn1 = new ToggleButton("rewind");
-        ToggleButton tBtn2 = new ToggleButton("pause");
-        ToggleButton tBtn3 = new ToggleButton("forward");
-        */
-
-        Button tBtn1 = new Button("rewind");
-        Button tBtn2 = new Button("pause");
-        Button tBtn3 = new Button("forward");
-        SplitMenuButton selectImg = new SplitMenuButton();
-        selectImg.setText("select image");
-
-        MenuItem o1 = new MenuItem("1");
-        MenuItem o2 = new MenuItem("2");
-        MenuItem o3 = new MenuItem("3");
-        MenuItem o4 = new MenuItem("4");
-        MenuItem o5 = new MenuItem("5");
-
-        selectImg.getItems().addAll(o1,o2,o3,o4,o5);
 
         /*
         ToggleGroup toggleGroup = new ToggleGroup();
@@ -127,6 +204,32 @@ public class Main extends Application {
             go();
             current.setImage(images.get(i));
         });
+        o0.setOnAction(actionEvent -> {
+            i=0;
+            current.setImage(images.get(i));
+            System.out.println(i);
+        });
+        o1.setOnAction(actionEvent -> {
+            i=1;
+            current.setImage(images.get(i));
+            System.out.println(i);
+        });
+        o2.setOnAction(actionEvent -> {
+            i=2;
+            current.setImage(images.get(i));
+            System.out.println(i);
+        });
+        o3.setOnAction(actionEvent -> {
+            i=3;
+            current.setImage(images.get(i));
+            System.out.println(i);
+        });
+        o4.setOnAction(actionEvent -> {
+            i=4;
+            current.setImage(images.get(i));
+            System.out.println(i);
+        });
+
 
         //functions
         /*
@@ -147,7 +250,7 @@ public class Main extends Application {
         selectr.getItems().addAll(c1, c2, c3);
 
         //Layout
-        HBox layout1 = new HBox(tBtn1, tBtn3, importBtn);
+        HBox layout1 = new HBox(selectImg,tBtn1, tBtn3, importBtn);
         //HBox layout1 = new HBox();
         HBox layout2 = new HBox(current);
 
@@ -166,8 +269,9 @@ public class Main extends Application {
     }
 
     public void go() {
-        System.out.println("Forward!");
-        if (i < imgNum) i++;
+        if (i < imgNum-1) i++;
+        if (i == imgNum-1) System.out.println("no more images!");
+        else System.out.println("Forward!");
         System.out.println(i);
         //while (Sgo) i++;
     }
@@ -178,9 +282,11 @@ public class Main extends Application {
     }
 
     public void back() {
-        System.out.println("Rewind!");
         if (i > 0) i--;
+        if (i == 0) System.out.println("no more images!");
+        else System.out.println("Rewind!");
         System.out.println(i);
         //while (Sback) i--;
     }
+
 }
